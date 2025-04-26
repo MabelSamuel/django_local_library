ae7d0fda436d6fc53898b8a154c93156c76f1d6b
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-04-26 22:47:13 +0100

    added admin

--- mytestsite/catalog/admin.py
+++ mytestsite/catalog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Book, BookInstance, Author, Language
+
+class BookInstance(admin.TabularInline):
+    model = Book
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInstance]
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Language)
+class BookInlineInstance(admin.TabularInline):
+    model = BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInlineInstance]
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
