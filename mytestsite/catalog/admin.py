47cf304ad064584cc9c0a07888d94763d3e8dd9a
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-05-03 22:21:32 +0100

    admin

--- mytestsite/catalog/admin.py
+++ mytestsite/catalog/admin.py
@@ -3,20 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Genre, Book, BookInstance, Author, Language
 
-class BookInstance(admin.TabularInline):
+"""Minimal registration of models
+ admin.site.register(Book)
+ admin.site.register(BookInstance)
+ admin.site.register(Author)
+ """
+admin.site.register(Genre)
+admin.site.register(Language)
+class BooksInline(admin.TabularInline):
     model = Book
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-    inlines = [BookInstance]
+    inlines = [BooksInline]
 
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Genre)
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
-# admin.site.register(Author)
-admin.site.register(Language)
 class BookInlineInstance(admin.TabularInline):
     model = BookInstance
 @admin.register(Book)
--- mytestsite/catalog/views.py
+++ mytestsite/catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Genre, Book, BookInstance, Author
+
+def index(request):
+    """View function for home page of site."""
+    
